887f0a95c54be3d7a593ce40bf3bdb9b26e456ba
nqd1 <nqd001205@gmail.com>
2025-03-25 00:22:21 +0700

    kms

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -1,5 +1,5 @@
 import { Message } from "@/types";
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
 import { ChatInput } from "./ChatInput";
 import { ChatLoader } from "./ChatLoader";
 import { ChatMessage } from "./ChatMessage";
@@ -13,13 +13,23 @@ interface Props {
 }
 
 export const Chat: FC<Props> = ({ messages, loading, onSend, onReset }) => {
+  const messagesEndRef = useRef<HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages, loading]);
+
   return (
-    <>
+    <div className="flex flex-col h-full">
       <div className="flex flex-row justify-between items-center mb-4 sm:mb-8">
         <ResetChat onReset={onReset} />
       </div>
 
-      <div className="flex flex-col rounded-lg px-2 sm:p-4 sm:border border-neutral-300">
+      <div className="flex-1 overflow-y-auto flex flex-col rounded-lg px-2 sm:p-4 sm:border border-neutral-300">
         {messages.map((message, index) => (
           <div
             key={index}
@@ -35,10 +45,12 @@ export const Chat: FC<Props> = ({ messages, loading, onSend, onReset }) => {
           </div>
         )}
 
-        <div className="mt-4 sm:mt-8 bottom-[56px] left-0 w-full">
-          <ChatInput onSend={onSend} />
-        </div>
+        <div ref={messagesEndRef} />
       </div>
-    </>
+
+      <div className="mt-4 sm:mt-8 w-full">
+        <ChatInput onSend={onSend} />
+      </div>
+    </div>
   );
 };
