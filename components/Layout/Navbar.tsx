aa58c10f5d7fc1327aec7cd6b5212769540a3be9
nqd1 <nqd001205@gmail.com>
2025-03-25 00:56:53 +0700

    hehe

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -6,7 +6,7 @@ export const Navbar: FC = () => {
     <div className="flex h-[50px] sm:h-[60px] border-b border-neutral-300 py-2 px-2 sm:px-8 items-center justify-between">
       <div className="font-bold text-3xl flex items-center">
         <a className="ml-2 flex items-center hover:opacity-50" href="https://www.facebook.com/SINNOclub/">
-          <Image src="/media/Sinno_logo.png" alt="logo" width={32} height={32} />
+          <img src="/media/Sinno_logo.png" alt="logo" width={32} height={32} />
           <span className="ml-2">Chatbot Sonni</span>
         </a>
       </div>
