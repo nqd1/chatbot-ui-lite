58ddb605852f53d1fa0aa6fc35a2dfdca4f6831b
nqd1 <nqd001205@gmail.com>
2025-03-25 13:25:16 +0700

    fml

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -6,8 +6,7 @@ export const Navbar: FC = () => {
     <div className="flex h-[50px] sm:h-[60px] border-b border-neutral-300 py-2 px-2 sm:px-8 items-center justify-between">
       <div className="font-bold text-3xl flex items-center">
         <a className="ml-2 flex items-center hover:opacity-50" href="https://www.facebook.com/SINNOclub/">
-          <img src="https://media.discordapp.net/attachments/1353588895245406253/1353792153607995392/SINNO_and_name.png?ex=67e2f0b1&is=67e19f31&hm=a575b94457cb72218487e49a12b70548bc1e98f56aa0b85f9029d700e867c583&=&format=webp&quality=lossless&width=940&height=940" 
-          alt="logo" width={40} height={40} />
+          <img src="/public/sinno_logo.png" alt="logo" width={40} height={40} />
           <span className="ml-2">Chatbot Sonni</span>
         </a>
       </div>
