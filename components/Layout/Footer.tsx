8a67ab2ae879febaabc8b3a4df1586fc598e96e3
nqd1 <nqd001205@gmail.com>
2025-03-26 00:16:15 +0700

    footer

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer: FC = () => {
   return (
     <footer className="py-4">
       <div className="max-w-[800px] mx-auto px-4 text-center text-neutral-500 text-sm">
-        <p>© 2024 AI Chatbot. All rights reserved.</p>
+        <p>SoICT Innovation Club - 1 sản phẩm phục vụ Innovation Day 2025</p>
       </div>
     </footer>
   );
