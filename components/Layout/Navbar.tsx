610aacee27be351fe958cced42869e7e302784a6
Nguyen Quy Duc <168408695+nqd1@users.noreply.github.com>
2025-03-25 13:33:36 +0700

    Update Navbar.tsx

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -6,8 +6,8 @@ export const Navbar: FC = () => {
     <div className="flex h-[50px] sm:h-[60px] border-b border-neutral-300 py-2 px-2 sm:px-8 items-center justify-between">
       <div className="font-bold text-3xl flex items-center">
         <a className="ml-2 flex items-center hover:opacity-50" href="https://www.facebook.com/SINNOclub/">
-          <img src="https://media.discordapp.net/attachments/1353588895245406253/1353792153607995392/SINNO_and_name.png?ex=67e39971&is=67e247f1&hm=0ffc9781d02410abe43294dd3e79d9df83aae639a364a85a4360811fd5dd59fb&=&format=webp&quality=lossless&width=940&height=940"
-           alt="logo" width={50} height={50} />
+          <img src="https://media.discordapp.net/attachments/1353588895245406253/1353792153607995392/SINNO_and_name.png?ex=67e2f0b1&is=67e19f31&hm=a575b94457cb72218487e49a12b70548bc1e98f56aa0b85f9029d700e867c583&=&format=webp&quality=lossless&width=940&height=940" 
+          alt="logo" width={40} height={40} />
           <span className="ml-2">Chatbot Sonni</span>
         </a>
       </div>
