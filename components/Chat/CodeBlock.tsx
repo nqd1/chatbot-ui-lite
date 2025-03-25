3b19758cc187820c47d9b440bd448eeb9d4af81c
nqd1 <nqd001205@gmail.com>
2025-03-25 19:30:56 +0700

    ffdjosfnasetdfgssdrghserdg

--- components/Chat/CodeBlock.tsx
+++ components/Chat/CodeBlock.tsx
@@ -1,7 +1,6 @@
 import { FC } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
-import type { SyntaxHighlighterProps } from 'react-syntax-highlighter';
 
 interface Props {
   language: string;
